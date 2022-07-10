a81d86f45c92274170059155ff3aaca29aa97a69
Ivan Sychev <funovichf@gmail.com>
2022-07-10 21:59:12 +0500

    cli is done

--- ascii_art/image.py
+++ ascii_art/image.py
@@ -1,21 +1,57 @@
 from PIL import Image
 
 
+def make_ascii_art(path, out_path, is_console_output, ascii_art_width, ascii_art_height, ascii_chars):
+    image = prepare_image_for_ascii_art(path, ascii_art_width, ascii_art_height)
+    ascii_art = to_ascii_art(image, ascii_chars)
+
+    if is_console_output:
+        print(ascii_art)
+    else:
+        write_ascii_art(ascii_art, path, out_path)
+
+
+def to_ascii_art(image, chars):
+    chars = list(set(chars))
+    pixels = image.getdata()
+
+    width, height = image.size
+
+    groups_count = int(255 / (len(chars) - 1))
+
+    new_pixels = [chars[pixel[0] // groups_count] for pixel in pixels]
+    new_pixels = ''.join(new_pixels)
+
+    new_pixels_count = len(new_pixels)
+    ascii_image = [new_pixels[index:index + width] for index in range(0, new_pixels_count, width)]
+    ascii_image = "\n".join(ascii_image)
+
+    return ascii_image
+
+
+def write_ascii_art(ascii_art, path, out_path):
+    if out_path is None:
+        out_path = path.split('.')[0]
+
+    with open(out_path + '.txt', "w") as file:
+        file.write(ascii_art)
+
+
 def prepare_image_for_ascii_art(image_path, new_width, new_height):
     img = Image.open(image_path).convert('RGB')
 
     width, height = img.size
 
-    new_width, new_height = get_new_size(width, height, new_width, new_height)
+    new_width, new_height = get_new_image_size(width, height, new_width, new_height)
 
     img = img.resize((new_width, new_height))
 
-    gray_img = to_gray_scale(img)
+    new_img = to_gray_scale(img)
 
-    return gray_img
+    return new_img
 
 
-def get_new_size(width, height, new_width, new_height):
+def get_new_image_size(width, height, new_width, new_height):
     if new_width is None and new_height is None:
         new_width = width
         new_height = height
---
+++ cli.py
@@ -0,0 +1,74 @@
+import argparse
+from ascii_art.image import make_ascii_art
+
+
+def create_parser():
+    description = "Из обычного изображения делает asсii-art"
+
+    parser = argparse.ArgumentParser(description=description)
+
+    parser.add_argument(
+        'path',
+        help='Путь до изображения для asсii-art',
+        type=str
+    )
+    parser.add_argument(
+        '--out_path',
+        '-op',
+        help='Путь для вывода asсii-art в файл, если он не указан, '
+             'то будет использован путь до исходного изображения',
+        type=str,
+        default=None
+    )
+    parser.add_argument(
+        '--console',
+        '-c',
+        help='Отключить вывод в файл и вывести ascii-art на консоль',
+        action='store_true')
+    parser.add_argument(
+        '--width',
+        '-w',
+        help='Ширина asсii-art изображения. Если указано, что-то одно '
+             '(ширина или высота), то второе будет посчитано на основе '
+             'пропорций исходного изображения. Если же не указано ничего, '
+             'что за ширину и высоту будут взяты исходные размеры',
+        type=int,
+        default=None)
+    parser.add_argument(
+        '--height',
+        '-he',
+        help='Высота asсii-art изображения. Если указано, что-то одно '
+             '(ширина или высота), то второе будет посчитано на основе '
+             'пропорций исходного изображения. Если же не указано ничего, '
+             'что за ширину и высоту будут взяты исходные размеры',
+        type=int,
+        default=None)
+    parser.add_argument(
+        '--chars',
+        '-ch',
+        help='Символы для asсii-art изображения',
+        type=str,
+        default='#&@$%')
+
+    return parser
+
+
+def main():
+    parser = create_parser()
+    cmd_commands = parser.parse_args()
+
+    try:
+        make_ascii_art(
+            cmd_commands.path,
+            cmd_commands.out_path,
+            cmd_commands.console,
+            cmd_commands.width,
+            cmd_commands.height,
+            cmd_commands.chars
+        )
+    except RuntimeError:
+        print('Что-то пошло не так, напишите разработчикам')
+
+
+if __name__ == '__main__':
+    main()
