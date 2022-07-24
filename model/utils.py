22a74819f58e890f48ea3f40786656168d3834ea
Ivan Sychev <funovichf@gmail.com>
2022-07-24 15:50:14 +0500

    fix utils

--- model/utils.py
+++ model/utils.py
@@ -2,10 +2,12 @@ import os
 
 
 def write_ascii_art(ascii_art, image_path, out_folder_path):
+    other_sep = '/' if os.sep == '\\' else '\\'
+
     if os.sep in image_path:
         image_name = image_path.split(os.sep)[-1]
     else:
-        image_name = image_path.split('/')[-1]
+        image_name = image_path.split(other_sep)[-1]
 
     file_name = image_name[:image_name.rfind('.')] + '.txt'
 
@@ -14,7 +16,7 @@ def write_ascii_art(ascii_art, image_path, out_folder_path):
     elif os.sep in out_folder_path:
         out_folder_path = os.path.join(out_folder_path, file_name)
     else:
-        out_folder_path += '/' + file_name
+        out_folder_path += other_sep + file_name
 
     with open(out_folder_path, "w") as file:
         file.write(ascii_art)
